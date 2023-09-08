0c8140d9f0d3cc7adf8481a4f4d483a7d0885f21
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-09-09 00:17:57 +0900

    スタイルをあてる前まで

---
+++ src/components/SendMessage.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { db, auth } from '../firebase';
+import firebase from 'firebase/compat/app';
+import { Input } from '@mui/material';
+import { Send } from '@mui/icons-material';
+
+
+function SendMessage() {
+
+  const [message , setMessages] = useState("");
+
+  //↓↓↓ 関数なのでconst ではなくて functionだよ
+  function sendMessage(e) {
+    e.preventDefault();      
+    //文字を入力してエンター押したときにレンダリングされてしまうのを防ぐ。
+    // レンダリングしてほしいんじゃなくて入力した情報を送りたいだけだから。
+
+    const {uid , photoURL} = auth.currentUser;
+
+
+    //firebaseで用意されている　db.collection("").add({})
+    db.collection("messages").add({
+      text:message,
+      photoURL,
+      uid,
+      createdAt:firebase.firestore.FieldValue.serverTimestamp(),  
+      //firebaseで用意されているもの。　　カンマで区切るの忘れないでね?
+
+      //createdAtをcreateAtと入力してしまっていたためデータが表示されないエラーに悩まされた
+      //気をつけて!!
+      
+    });
+
+    setMessages("");
+
+  }
+
+
+
+  return (
+    <div>
+      <form onSubmit={sendMessage}>
+        <div className='sendMsg'>
+          <Input placeholder='メッセージを入力してください' 
+                 type='text' 
+                 onChange={(e) => setMessages(e.target.value)}
+                 value = {message}
+          />
+          <Send onClick={sendMessage} style={{cursor:"pointer"}}/>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SendMessage
